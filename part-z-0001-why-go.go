7d825dadba19f789504e0dc1774b1969c688f1a0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-18 12:08:50 +0300

    Create part-z-0001-why-go.go

---
+++ part-z-0001-why-go.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Step by step, I'll learn and use Golang. Why? For faster AI/Web3 apps...")
+	
+	fmt.Println("Mustafa Buyukdereli was here!")
+	
+	fmt.Println("A rolling stone gathers no moss...")
+}
